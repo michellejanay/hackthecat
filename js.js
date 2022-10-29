f1c2ca183d4d471de17f4653675c09ecc4aa7bcc
Chelle <dearmichelley@gmail.com>
2022-10-29 20:37:24 +0100

    hacked: cats

--- js.js
+++ js.js
@@ -1,21 +1,22 @@
-const clicker = document.getElementById("clicker")
-const clicker2 = document.getElementById("clicker2")
-const img = document.getElementById("carousel")
-const catImgs = document.getElementById("cat-imgs")
-let counter = 0
-console.log(counter)
-clicker.addEventListener("click", () =>{
-    catImgs.location.reload();
+const clicker = document.getElementById("clicker");
+const clicker2 = document.getElementById("clicker-pg-2");
+const img = document.getElementById("carousel");
+const catImgs = document.getElementById("cat-imgs");
 
-       counter++
-        if(clicker.src === '/image/image(1).png') {
-            clicker.style.display = 'none'
-            clicker2.style.display = 'flex'
-                 } else {
-           clicker2.style.display = 'none'
-            clicker.style.display = 'flex'
-         }
-})
-console.log(clicker.src)
+// clicker.addEventListener("click", () => {
+//   //catImgs.location.reload();
+//   clicker.style.display = "none";
+//   clicker2.style.display = "flex";
+// });
 
+// clicker2.addEventListener("click", () => {
+//   const catImgs = document.getElementById("cat-imgs");
+//   const content = "https://cataas.com/cat/gif";
+//   catImgs.src = "";
+//   catImgs.src = content;
+// });
 
+// console.log(clicker.src);
+clicker2.addEventListener("click", () => {
+  window.location.reload();
+});
