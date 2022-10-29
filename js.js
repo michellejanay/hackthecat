fb6940195d176e76d1f323b1790bb79032ef4ed9
Chelle <dearmichelley@gmail.com>
2022-10-29 22:25:26 +0100

    unneccessary comments: removed

--- js.js
+++ js.js
@@ -1,22 +1,5 @@
-const clicker = document.getElementById("clicker");
 const clicker2 = document.getElementById("clicker-pg-2");
-const img = document.getElementById("carousel");
-const catImgs = document.getElementById("cat-imgs");
 
-// clicker.addEventListener("click", () => {
-//   //catImgs.location.reload();
-//   clicker.style.display = "none";
-//   clicker2.style.display = "flex";
-// });
-
-// clicker2.addEventListener("click", () => {
-//   const catImgs = document.getElementById("cat-imgs");
-//   const content = "https://cataas.com/cat/gif";
-//   catImgs.src = "";
-//   catImgs.src = content;
-// });
-
-// console.log(clicker.src);
 clicker2.addEventListener("click", () => {
   window.location.reload();
 });
